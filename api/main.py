efba5b1050de559a3d82fe089d11511e1f15a8c8
kartikya2522 <kartikyag25@gmail.com>
2026-08-09 17:15:15 +0530

    Fix CORS to allow Vercel frontend domain

--- api/main.py
+++ api/main.py
@@ -68,7 +68,10 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://geoai-fire-intelligence.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
